e0874bb9a1c966c8b80a2548e4ab34b8d0c198c7
OPKovalev <kovalyovop@yandex.ru>
2026-06-26 16:56:35 +0300

    Start

---
+++ src/main/java/com/logistic/edo/edos_platform/EdosPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.logistic.edo.edos_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdosPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdosPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logistic/edo/edos_platform/EdosPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistic.edo.edos_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdosPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
